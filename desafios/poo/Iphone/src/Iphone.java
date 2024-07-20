73a4e4508965cf2589f0a2e28ccb10eec96f9471
pedrobarroso01 <pedrobarrosocontato@gmail.com>
2024-07-19 21:20:03 -0300

    Desafio POO

---
+++ desafios/poo/Iphone/src/Iphone.java
@@ -0,0 +1,69 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorNaInternet {
+
+    // Métodos de ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Métodos de AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    // Métodos de NavegadorNaInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+        // Testando funcionalidades do Reprodutor Musical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("High - Japa");
+
+        // Testando funcionalidades do Aparelho Telefônico
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando funcionalidades do Navegador na Internet
+        meuIphone.exibirPagina("http://www.google.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
